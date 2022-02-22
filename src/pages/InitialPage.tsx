4bf41484124002ae0f4f3ee5550ade1cec5fad36
Paulo <paulofreire@frwk.com.br>
2022-02-22 19:22:46 -0300

    add css and sass

--- src/pages/InitialPage.tsx
+++ src/pages/InitialPage.tsx
@@ -1,12 +1,28 @@
+import { FormEvent, useState } from "react";
+import api from "../services/api";
+import "../styles/style.css";
+
+interface User {
+  full_name: string;
+  description: string;
+  user_repositories_url: string;
+}
+
 export default function InitialPage() {
+  const [user, setUser] = useState("");
+
   return (
-    <div>
-      <h1>Repositórios Github</h1>
-      <p>Localize repositórios por usuário</p>
-      <input type="text" placeholder="Digite nome de usuário"></input>
-      <a href="/list" type="button">
-        Pesquisar
-      </a>
+    <div className="page">
+      <h1>Explore Repositórios no Github</h1>
+
+      <form action="/list">
+        <input
+          value={user}
+          onChange={(e) => setUser(e.target.value)}
+          placeholder="Digite o nome do usuário"
+        />
+        <button type="submit">Pesquisar</button>
+      </form>
     </div>
   );
 }
