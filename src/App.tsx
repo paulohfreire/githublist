ada3977c06c4b068e9ebc3db026090ab56456be0
Paulo <paulofreire@frwk.com.br>
2022-02-24 15:24:06 -0300

    change page and apply hooks to list user's repositories

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import InitialPage from "./pages/InitialPage";
-import ListPage from "./pages/ListPage";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<InitialPage />} />
-        <Route path="/list" element={<ListPage />} />
       </Routes>
     </div>
   );
--- src/pages/InitialPage.tsx
+++ src/pages/InitialPage.tsx
@@ -1,28 +1,62 @@
-import { FormEvent, useState } from "react";
-import api from "../services/api";
 import "../styles/style.css";
+import { useEffect, useState } from "react";
 
-interface User {
-  full_name: string;
-  description: string;
-  user_repositories_url: string;
-}
+export default function ListPage() {
+  const [repos, setRepos] = useState<any[]>([]);
+  const [values, setValues] = useState({ repository: "" });
 
-export default function InitialPage() {
-  const [user, setUser] = useState("");
+  useEffect(() => {
+    fetchRepos();
+  }, [values.repository]);
+
+  const fetchRepos = async (user: string) => {
+    const repos = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=50`
+    );
+
+    const data = await repos.json();
+    setRepos(data);
+  };
+
+  const handleSubmit = () => {
+    if (!values.repository) return;
+    else if (values.repository == undefined)
+      return console.error("Não encontrado");
+    fetchRepos(values.repository);
+  };
+
+  const inputChange = (e: { target: { name: any; value: any } }) => {
+    const { name, value } = e.target;
+
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
 
   return (
-    <div className="page">
-      <h1>Explore Repositórios no Github</h1>
+    <div>
+      <h1>Lista dos repositórios do usuário</h1>
+      <input
+        name="repository"
+        onChange={inputChange}
+        placeholder="digite o nome do seu usuário"
+        value={values.repository}
+      />
 
-      <form action="/list">
-        <input
-          value={user}
-          onChange={(e) => setUser(e.target.value)}
-          placeholder="Digite o nome do usuário"
-        />
-        <button type="submit">Pesquisar</button>
-      </form>
+      <button type="button" onClick={handleSubmit}>
+        Buscar
+      </button>
+
+      <ul>
+        {repos.map((repo) => {
+          return (
+            <li key={repo.id}>
+              <p>{repo.name}</p>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
